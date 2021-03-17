0ffacbd2a8acf9ea804c05c3fd3424f78686adbb
pierregober <pierregober@gmail.com>
2021-03-17 09:44:13 -0700

    recursive function to gather all sites int the mef

--- v2/sitePermissions.js
+++ v2/sitePermissions.js
@@ -1,10 +1,10 @@
-function getSiteGroups() {
+var allSites = [];
+
+function initGetSubsite() {
   var siteurl = _spPageContextInfo.siteAbsoluteUrl;
 
   $.ajax({
-    url:
-      siteurl +
-      "/_api/web/getsubwebsfilteredforcurrentuser(nWebTemplateFilter=-1,nConfigurationFilter=-1)?$select=title,ServerRelativeUrl",
+    url: siteurl + "/_api/web/webinfos?$select=ServerRelativeUrl,Title",
     method: "GET",
     headers: {
       Accept: "application/json; odata=verbose",
@@ -12,16 +12,54 @@ function getSiteGroups() {
       "X-RequestDigest": $("#__REQUESTDIGEST").val(),
     },
     success: function (data) {
-      console.log("Here's your data:", data.d);
-      getUsersByGroup(data);
+      data.d.results.map(function (props) {
+        allSites.push(props);
+        getSubSites(props.ServerRelativeUrl, props.Title);
+      });
+      getSitePermissions();
     },
     error: function (error) {
       console.log("Error retrieving UsageLog: " + JSON.stringify(error));
     },
   });
 }
-getSiteGroups();
-function getUsersByGroup(data) {}
+initGetSubsite();
+
+//Recursion to get subsites additonal sites
+function getSubSites(SubSiteUrl, SubSiteTitle) {
+  $.ajax({
+    url:
+      "https://intelshare.intelink.gov" +
+      SubSiteUrl +
+      "/_api/web/webinfos?$select=ServerRelativeUrl,Title",
+    method: "GET",
+    headers: {
+      Accept: "application/json; odata=verbose",
+    },
+    success: function (subsites) {
+      //get rid of empty arrays
+      if (subsites.d.results.length > 0) {
+        //break down an array for the collector
+        if (subsites.d.results.length >= 1) {
+          subsites.d.results.forEach(function (site, index) {
+            allSites.push(site);
+          });
+        } else {
+          allSites.push(subsites.d.results);
+        }
+      }
+      subsites.d.results.map(function (props) {
+        getSubSites(props.ServerRelativeUrl, props.Title);
+      });
+    },
+    error: function (subsites) {},
+    async: false,
+  });
+}
+
+function getSitePermissions() {
+  console.log(allSites);
+}
 
 //get all group permissions
 //get all people related to that group
