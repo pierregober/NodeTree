bf45f0eefab78c1ebeda75ed69b9a7d7bd7ec8c4
pierregober <pierregober@gmail.com>
2021-03-22 10:01:18 -0700

    formatting data for the reducer

--- v2/web-workers/sitePermissions.js
+++ v2/web-workers/sitePermissions.js
@@ -1,98 +1,40 @@
-var allSites = [];
-var allSitesPermissions = [];
-var workerValues = {};
-//make shorter pierre
-var worker = new Worker(
-  "https://intelshare.intelink.gov/sites/imef/imo/StarterSite/SiteAssets/ims/worker.js"
-);
+//wrap in a iffe to avoid making globally avialable variables.
+// "use strict";
+(function () {
+  var allSitesPermissions = [];
+  //make shorter pierre
+  var workerUrl =
+    "https://intelshare.intelink.gov/sites/imef/imo/StarterSite/SiteAssets/ims/worker.js";
+  var sharePointWorker = new Worker(workerUrl);
 
-function initGetSubsite() {
-  var siteurl = _spPageContextInfo.siteAbsoluteUrl;
-  $.ajax({
-    url: siteurl + "/_api/web/webinfos?$select=ServerRelativeUrl,Title",
-    method: "GET",
-    headers: {
-      Accept: "application/json; odata=verbose",
-      "content-type": "application/json; odata=verbose",
-      "X-RequestDigest": $("#__REQUESTDIGEST").val(),
-    },
-    success: function (data) {
-      console.log("data before the workers:", data);
-      //add addEventListener to worker
-      worker.addEventListener("message", function (e) {});
-      data.d.results.map(function (props) {
-        //push the parent to the collector
-        allSites.push(props);
-        workerGetSubSites(props);
-      });
-      //getPermissions();
-    },
-    error: function (error) {
-      console.log("Error retrieving UsageLog: " + JSON.stringify(error));
-    },
+  sharePointWorker.postMessage({
+    requestDigest: document.getElementById("__REQUESTDIGEST").value,
+    qs1: "/_api/web/webinfos?$select=ServerRelativeUrl,Title",
+    qs2:
+      "/_api/Web/RoleAssignments?$expand=Member/Users,RoleDefinitionBindings",
+    url: _spPageContextInfo.siteAbsoluteUrl,
   });
-}
-initGetSubsite();
 
-function workerGetSubSites(props) {
   //spawn a worker
-  workerValues.requestDigest = $("#__REQUESTDIGEST").val();
-  workerValues.url = props.ServerRelativeUrl;
-  worker.postMessage(workerValues);
-  //get worker results
-  worker.onmessage = function (e) {
-    console.log("heres the event:", e.data, "\n allsites: ", allSites);
-    //get rid of empty arrays
-    if (e.data.length > 0) {
-      //break down an array for the collector
-      if (e.data.length >= 1) {
-        e.data.forEach(function (site) {
-          //add parent to collector
-          allSites.push(site);
-          //spawn new worker
-          workerValues.requestDigest = $("#__REQUESTDIGEST").val();
-          workerValues.url = site.ServerRelativeUrl;
-          worker.postMessage(workerValues);
-        });
-      } else {
-        allSites.push(e.data);
-      }
+  sharePointWorker.onmessage = function (e) {
+    allSitesPermissions = allSitesPermissions.concat([
+      { pathname: e.data.url, groups: e.data.permissions },
+    ]);
+
+    e.data.sites.forEach(function (props) {
+      sharePointWorker.postMessage({
+        requestDigest: document.getElementById("__REQUESTDIGEST").value,
+        qs1: "/_api/web/webinfos?$select=ServerRelativeUrl,Title",
+        qs2:
+          "/_api/Web/RoleAssignments?$expand=Member/Users,RoleDefinitionBindings",
+        url: "https://intelshare.intelink.gov" + props.url,
+      });
+    });
+    //TESTING you must change
+    //will need to format the data after the last permission makes it
+    if (allSitesPermissions.length === 53) {
+      console.log("allSitesPermissions:", allSitesPermissions);
+      reduceData(allSitesPermissions);
     }
   };
-}
-function getPermissions() {
-  allSites.map(function (props, index) {
-    var siteKey = props.ServerRelativeUrl.toLowerCase();
-    $.ajax({
-      url:
-        "https://intelshare.intelink.gov" +
-        props.ServerRelativeUrl +
-        "/_api/Web/RoleAssignments?$expand=Member/Users,RoleDefinitionBindings",
-      method: "GET",
-      headers: {
-        Accept: "application/json; odata=verbose",
-        "content-type": "application/json; odata=verbose",
-        "X-RequestDigest": $("#__REQUESTDIGEST").val(),
-      },
-      success: function (data) {
-        var obj = {};
-        obj[siteKey] = data.d.results;
-        allSitesPermissions.push(obj);
-        //just testing to move to move to next function
-        if (allSites.length - 1 == index) {
-          allSitesPermissions = allSitesPermissions.sort();
-          next();
-        }
-      },
-      error: function (error) {
-        console.log(
-          "Error retrieving site permissions: " + JSON.stringify(error)
-        );
-      },
-    });
-  });
-}
-function next() {
-  console.log("allSites array:", allSites);
-  console.log("allSitesPermissions array:", allSitesPermissions);
-}
+})();
--- v2/web-workers/worker.js
+++ v2/web-workers/worker.js
@@ -1,31 +1,57 @@
-self.addEventListener("message", function (e) {
-  //avoid accepting other workers
-  if (e.data && e.data.url) {
-    // self.postMessage(e.data + " test");
-    //get the subsite and its childern
-    // console.log(JSON.stringify(e.data));
-    fetch(
-      "https://intelshare.intelink.gov" +
-        e.data.url +
-        "/_api/web/webinfos?$select=ServerRelativeUrl,Title",
-      {
-        method: "GET",
-        headers: {
-          Accept: "application/json; odata=verbose",
-          "Content-type": "application/json; odata=verbose",
-          "X-RequestDigest": e.data.requestDigest,
-        },
-      }
-    )
+// "use strict";
+
+function formatSites(props) {
+  return {
+    url: props.ServerRelativeUrl,
+  };
+}
+function formatPermissions(props) {
+  return {
+    name: props.Member.Title,
+    members: (props.Member.Users?.results || []).map(function (member) {
+      return {
+        email: member.Email,
+        id: member.Id,
+        isAdmin: member.IsSiteAdmin,
+        title: member.Title,
+      };
+    }),
+  };
+}
+
+function _getRequest(props) {
+  return new Promise(function (resolve, reject) {
+    fetch(props.url + props.qs, {
+      method: "GET",
+      headers: {
+        Accept: "application/json; odata=verbose",
+        "Content-type": "application/json; odata=verbose",
+        "X-RequestDigest": props.requestDigest,
+      },
+    })
       .then(function (resp) {
         return resp.json();
       })
-      .then(function (data) {
-        var sites = data.d.results;
-        self.postMessage(sites);
+      .then(resolve)
+      .catch(reject);
+  });
+}
+
+self.addEventListener("message", function (e) {
+  if (!e.data.data) {
+    var promise1 = _getRequest(Object.assign(e.data, { qs: e.data.qs1 }));
+    var promise2 = _getRequest(Object.assign(e.data, { qs: e.data.qs2 }));
+
+    Promise.all([promise1, promise2])
+      .then(function ([data1, data2]) {
+        self.postMessage({
+          permissions: data2.d.results.map(formatPermissions),
+          sites: data1.d.results.map(formatSites),
+          url: e.data.url,
+        });
       })
       .catch(function (e) {
-        self.postMessage(e + "test");
+        console.log("Error in Promise: ", e);
       });
   }
 });
